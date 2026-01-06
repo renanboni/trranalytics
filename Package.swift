4ea5bd09f352aedb23fac2f07e71922dfb7db152
renan.boni <r.boni1994@gmail.com>
2026-01-06 16:44:11 -0300

    commiting xcframework

--- Package.swift
+++ Package.swift
@@ -1,9 +1,6 @@
 // swift-tools-version:5.9
 import PackageDescription
 
-let version = "1.0.6"
-let checksum = "bd545a7c9aaeb4b97c5e8ee6703a5d41cb51ada0dc34d51588b955aa943243f5"
-
 let package = Package(
     name: "TRRAnalytics",
     platforms: [
@@ -18,8 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Shared",
-            url: "git@github.com:TheRealReal/trr-analytics/releases/download/v\(version)/Shared.xcframework.zip",
-            checksum: checksum
+            path: "Shared.xcframework"
         )
     ]
 )
